e0adb6288cda8911d55f4900b37044abf3b338c6
Anjula Tamang <parkhrinj@gmail.com>
2023-04-22 12:10:52 -0400

    created routes, forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterForm from "./registerForm";
+import NewOrderForm from "./newOrderForm";
+import NavBar from "./navBar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route>
+          <Route path="/" element ={<NavBar/>}/>
+          <Route path="/registerform" element={<RegisterForm />} />
+          <Route path="/neworderform" element={<NewOrderForm />} />
+        </Route>
+      </Routes>
   );
 }
 
-export default App;
